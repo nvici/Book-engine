c24a28026ef14e4ce6d0fce4c5aaabb5d1cae450
nvici <nvicinanza@outlook.com>
2023-01-23 18:15:14 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://136.244.41.17/32/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
